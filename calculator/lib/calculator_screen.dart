ba82ccfd5a705394d32e67717165d27d35dc6e84
Anwesa Ghosh <anwesa2021@gmail.com>
2024-08-07 11:33:16 +0530

    wrong

---
+++ calculator/lib/calculator_screen.dart
@@ -0,0 +1,98 @@
+import 'package:calculator/button_values.dart';
+import 'package:flutter/material.dart';
+
+class CalculatorScreen extends StatefulWidget {
+  const CalculatorScreen({super.key});
+
+  @override
+  State<CalculatorScreen> createState() => _CalculatorScreenState();
+}
+
+class _CalculatorScreenState extends State<CalculatorScreen> {
+  String number1 = "";
+  String operand = "";
+  String number2 = "";
+
+  @override
+  Widget build(BuildContext context) {
+    final screenSize = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Column(
+        children: [
+          //output
+          Expanded(
+            child: SingleChildScrollView(
+              reverse: true,
+              child: Container(
+                alignment: Alignment.bottomRight,
+                padding: const EdgeInsets.all(16),
+                child: Text(
+                  "$number1$operand$number2".isEmpty
+                      ? "0"
+                      : "$number1$operand$number2",
+                  style: const TextStyle(
+                    fontSize: 48,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  textAlign: TextAlign.end,
+                ),
+              ),
+            ),
+          ),
+
+          //buttons
+          Wrap(
+            children: Btn.buttonValues
+                .map(
+                  (value) => SizedBox(
+                      width: value == Btn.n0
+                          ? screenSize.width / 2
+                          : (screenSize.width / 4),
+                      height: screenSize.width / 5,
+                      child: buildButton(value)),
+                )
+                .toList(),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget buildButton(value) {
+    return Padding(
+      padding: const EdgeInsets.all(4.0),
+      child: Material(
+        color: getBtnColor(value),
+        clipBehavior: Clip.hardEdge,
+        shape: OutlineInputBorder(
+          borderSide: const BorderSide(
+            color: Colors.white24,
+          ),
+          borderRadius: BorderRadius.circular(100),
+        ),
+        child: InkWell(
+            onTap: () {},
+            child: Center(
+                child: Text(
+              value,
+              style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+            ))),
+      ),
+    );
+  }
+
+  getBtnColor(value) {
+    [Btn.del, Btn.clr].contains(value)
+        ? Colors.blueGrey
+        : [
+            Btn.per,
+            Btn.multiply,
+            Btn.add,
+            Btn.subtract,
+            Btn.divide,
+            Btn.calculate
+          ].contains(value)
+            ? Colors.orange
+            : Colors.black87;
+  }
+}
